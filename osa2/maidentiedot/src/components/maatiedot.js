b510343461c3278829b2efb8396952f9562e3a9d
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-02-02 17:17:27 +0200

    2.18-2.20 Done

--- osa2/maidentiedot/src/components/maatiedot.js
+++ osa2/maidentiedot/src/components/maatiedot.js
@@ -5,6 +5,8 @@ import kelvinToCelsius from "../asteet/kelvin"
 const Maantiedot = ({country}) => {
 
     const [temperature, setTemperature] = useState(0)
+    const [wind, setWind]=useState(0)
+    const [icon, setIcon]=useState('')
 
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${
         process.env.REACT_APP_API_KEY}`)
@@ -14,6 +16,8 @@ const Maantiedot = ({country}) => {
                 Math.round(kelvinToCelsius(response.data.main.temp)*10)/10
                 
             )
+            setWind(response.data.wind.speed)
+            setIcon(response.data.weather[0].icon)
         })
     return(
         
@@ -29,6 +33,8 @@ const Maantiedot = ({country}) => {
     <div><img src={country.flags.png} alt={`${country.name} flag `}/> </div>
     <h2>Weather in {country.capital[0]}</h2>
     <div>temperature {temperature} Celsius</div>
+    <img src = {`http://openweathermap.org/img/wn/${icon}@2x.png`} alt=" " />
+    <div>wind {wind}</div>
     </>
 
 
